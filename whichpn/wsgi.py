f43d773dbc74833557826139bda0321cd3a2fd73
Eugene <ti.eugene@gmail.com>
2023-03-26 04:32:20 +0300

    Apache ok, local_settings ok

--- whichpn/wsgi.py
+++ whichpn/wsgi.py
@@ -1,8 +1,10 @@
 """WSGI config for WhichPN project."""
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 application = get_wsgi_application()
